1dee5fee933a4569888e699fc86e7b120a49d38f
ramesh7128 <ramesh7128@gmail.com>
2016-06-29 06:43:52 +0530

    heroku setting changes

--- killjoy/wsgi.py
+++ killjoy/wsgi.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "killjoy.settings")
 
+from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
