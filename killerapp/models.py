ec4fd5b07e031a936bc1a8b7c6bc4bc9f7435748
ramesh7128 <ramesh7128@gmail.com>
2016-07-01 00:18:05 +0530

    what we do content

--- killerapp/models.py
+++ killerapp/models.py
@@ -1,6 +1,21 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import RegexValidator
+phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
 
 # Create your models here.
+
+class SpoilerVictims(models.Model):
+
+	name = models.CharField(max_length=100, blank=False, null=False)
+	tvshow = models.CharField(max_length=100,blank=False, null=False)
+	phoneno = models.CharField(max_length=15, validators=[phone_regex], blank=False, null=False)
+
+	def __unicode__(self):
+		if self.name and self.tvshow:
+			return self.name + " " + self.tvshow
+		else:
+			return "Anonymous"	
+
 
--- killerapp/views.py
+++ killerapp/views.py
@@ -10,4 +10,13 @@ def homepage(request):
 	return render(request, 'index.html', {})
 
 def subscribe(request):
+	if request.method == 'POST':
+		token = request.POST.get('stripeToken')
+		name = request.POST.get('InputName')
+		phoneno = request.POST.get('Inputphno')
+		tvshow = request.POST.get('InputTvshow')
+		print token, "token", name, "name", phoneno, "tvshow", tvshow
+		
+		
+
 	return render(request, 'subscribe.html', {})
